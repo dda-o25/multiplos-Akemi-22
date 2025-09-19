c0ed22cc689c9755fbe8468c04cca25a5c0ef472
github_username <email_address>
2025-09-18 23:49:12 -0600

    primer intento

--- multiplos.py
+++ multiplos.py
@@ -1,15 +1,26 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Akemi Clarissa Olvera Arao
+19/09/25
+Con dos números, verificar si uno es múltiplo del otro
 """
 
 # Declaraciones
-CONSTANTE = valor
+
 
 # Entradas
-entrada = input()
+numero_1 = int(input("Ingrese un número: "))
+numero_2 = int(input("Ingrese otro número: "))
 
 # Proceso
+if numero_1 % numero_2 == 0:
+# Salida
+    print("El número ", numero_1, "es múltiplo del número ", numero_2)
+# Proceso
+elif numero_2 % numero_1 == 0:
+# Salida
+    print("El número ", numero_2, "es múltiplo del número ", numero_1)
+else: 
+# Salida
+    print("Ninguno de los números es múltiplo del otro")
 
 
-# Salidas
-print(salida)
